f54c5e645b90930e386c9e5801d8c3a814b5627e
rajonahmed749 <raazon749@gmail.com>
2021-05-24 20:52:04 +0600

    readme file edited

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { createContext, useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -6,23 +7,34 @@ import {
 } from "react-router-dom";
 import Home from './Components/Home/Home/Home';
 import Appointment from './Components/Appointment/Appointment/Appointment';
+import Login from './Components/Login/Login/Login';
+
+
+
+
+export const UserContext = createContext()
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({})
   return (
-    <Router>
-    <Switch>
-    <Route exact path="/">
-      <Home/>
-    </Route>
-    <Route path="appointment">
-      <Appointment></Appointment>
-    </Route>
-    <Route path="/appointment">
-      <Appointment></Appointment>
-     </Route>
-  </Switch>
-
-</Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="appointment">
+            <Appointment></Appointment>
+          </Route>
+          <Route path="/appointment">
+            <Appointment></Appointment>
+          </Route>
+          {/* <Route path="/login">
+            <Login></Login>
+          </Route> */}
+        </Switch>
+     </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/Login/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCR2yJss2KppsRIGUIw24HpwzkdyFLyxKM",
+    authDomain: "jamuna-diagonistic-center.firebaseapp.com",
+    projectId: "jamuna-diagonistic-center",
+    storageBucket: "jamuna-diagonistic-center.appspot.com",
+    messagingSenderId: "70162980811",
+    appId: "1:70162980811:web:d97f9b05b6c7b23a66204e"
+  };
+
+  export default firebaseConfig;
